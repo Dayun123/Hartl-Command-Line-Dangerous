49a5c0d6fe49e21c71b3802fac0cdbdbd947da38
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2018-04-01 12:52:57 -0500

    Add notes for section 1.1

---
+++ notes.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Notes from Hartl's "Learn Enough Command Line to be Dangerous" tutorial
+
+# ************************** 1.1: Running a Terminal **********************#
+
+# In order to work with the command line, we will need to open a terminal. I'm using Linux Mint, so in order to open a terminal on my machine I need to use the following keyboard shorcut:
+
+# ctrl-alt-T
+
+# The anatomy of a command line: (I'm using zsh with Oh-My-ZSH, so this is different than a bash terminal)
+
+# prompt  dir_name                  command -option argument
+# ➜       learn-enough-command-line rm      -f      foo.txt
+
+# Exercices
+
+# 1. Identify the prompt, command, options and arguments in the lines below.
+
+# [~]$ cd ruby
+# [~]$ = prompt, cd = command, ruby = argument
+
+# [ruby]$ ls -a
+# [ruby]$ = prompt, ls = command, -a = argument
+
+# [ruby]$ rm -f .DS_Store
+# [ruby]$ = prompt, rm = command, -f = option, .DS_Store = argument
+
+# [ruby]$ ls -a
+# [ruby]$ = prompt, ls = command, -a = option
+
+# [ruby]$ cd projects/
+# [ruby]$ = prompt, cd = command, projects/ = argument
+
+# [projects]$ pwd
+# [projects]$ = prompt, pwd = command
+
+# [projects]$
+# [projects]$ = prompt
+
+# 2. Figure out the keyboard shortcut for creating a new tab in the terminal.
+
+# ctrl-shift-T
+
+# *************************** 1.2: Our First Command ********************** #
+
+
+
+
+
+
+
